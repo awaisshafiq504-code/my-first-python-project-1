d47298e978282199ae4d84cfd1e3452daef75c5a
Awais shafiq <awaisshafiq504@gmail.com>
2026-08-16 20:07:42 +0200

    Add distance traveled calculator

---
+++ distance_traveled.py
@@ -0,0 +1,9 @@
+# Distance Traveled Calculator
+# This program claculates the distance is given velocity and time
+# def calculate_distance(velocity, time):
+#     distance = velocity * time
+#     return distance
+v = float(input("Velocity (m/s): "))
+t = float(input("Time (s): "))
+d = v * t  # Using the formula d = v * t
+print(f"The distance traveled is {d} meters.")
