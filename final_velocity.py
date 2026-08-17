846d6233d3f4cbd7fb7b6b9f88d559b19e4dc272
Awais shafiq <awaisshafiq504@gmail.com>
2026-08-17 19:14:12 +0200

    Improve final velocity speed detection

--- final_velocity.py
+++ final_velocity.py
@@ -12,5 +12,11 @@ T= float(input("Time (s): "))
 
 V = calculate_final_velocity(U, A, T)
 print(f"The final velocity is {V} m/s.")
+if abs(V) > abs(U):
+         print("Speeding up")
+elif abs(V) < abs(U):
+        print("Slowing down")
+else:
+            print("constant speed")
 
 print("PROGRAM FINISHED")
