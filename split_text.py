b5be9f187451b8d2ea8440012a97afb60d868eb8
Awais shafiq <awaisshafiq504@gmail.com>
2026-08-26 20:47:21 +0200

    Add enumerate practice with paragraph

--- split_text.py
+++ split_text.py
@@ -1,16 +1,6 @@
 # Split Text
-experiment = "Python Scientific Data Analysis"
+experiment = "Python is useful for Scientific Data Analysis because we can process measurements and find patterns."
 words = experiment.split()
-word_count = len(words)
-first_word = words[0]
-print(f"First word: {first_word}")
-second_word = words[1]
-print(f"Second word: {second_word}")
-third_word = words[2]
-print(f"Third word: {third_word}")
-four_word = words[3]
-print(f"Four word: {four_word}")
-print(words)
-print(f"Number of words: {word_count}")
-for word in words:
-    print(word)
+print(len(words))
+for number, word in enumerate(words, start=1):
+        print(f"{number},{word}")
