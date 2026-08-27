bb1b7f2b257711d39ce0aa6f4b162e4a33899c4d
Awais shafiq <awaisshafiq504@gmail.com>
2026-08-27 21:02:08 +0200

    Add string parsing and word counting practice

--- split_text.py
+++ split_text.py
@@ -1,6 +1,21 @@
 # Split Text
 experiment = "Python is useful for Scientific Data Analysis because we can process measurements and find patterns."
 words = experiment.split()
-print(len(words))
+print(words)
+# Count all words
+word_count = 0
+for word in words:
+    word_count = word_count + 1
+print(f"Number of words: {word_count}")
+
+# Number of each word
 for number, word in enumerate(words, start=1):
         print(f"{number},{word}")
+# Count the word "Data"
+data_count = 0
+
+for word in words:
+      if word == "Data":
+            data_count = data_count+1
+print(f"Data appears: {data_count} time(s)")
+            
